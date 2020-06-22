42e14bdbeb1052820b2a2a01b3864aed15c283ec
Virgile Daugé <virgile.dauge@loria.fr>
2020-06-22 11:55:52 +0200

    maj serv

--- to_deploy.py
+++ to_deploy.py
@@ -1,8 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 @app.route('/hello.json')
-def hello_world():
+def hello_json():
     return {'msg': 'Hello, World!'}
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
