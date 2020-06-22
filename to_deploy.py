95002eeec2ecf13f28937f7b9c485184364e64d4
Virgile Daugé <virgile.dauge@loria.fr>
2020-06-22 12:09:37 +0200

    ajout requete post

--- to_deploy.py
+++ to_deploy.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/hello.json')
+@app.route('/update', methods=['POST'])
+def update():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return {'coucou': 'coucoutext'}
+
+@app.route('/hello')
 def hello_json():
     return {'msg': 'Hello, World!'}
 if __name__ == "__main__":
